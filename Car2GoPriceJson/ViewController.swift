feb2c9000ac099faba87ce57053f0a431bcb5e5b
Hennnnes <Hroemmer@googlemail.com>
2015-08-29 16:10:30 +0200

    Initial Commit

---
+++ Car2GoPriceJson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Car2GoPriceJson
+//
+//  Created by Hennes Roemmer on 29.08.15.
+//  Copyright © 2015 Hennes Roemmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Car2GoPriceJsonTests/Car2GoPriceJsonTests.swift
@@ -0,0 +1,36 @@
+//
+//  Car2GoPriceJsonTests.swift
+//  Car2GoPriceJsonTests
+//
+//  Created by Hennes Roemmer on 29.08.15.
+//  Copyright © 2015 Hennes Roemmer. All rights reserved.
+//
+
+import XCTest
+@testable import Car2GoPriceJson
+
+class Car2GoPriceJsonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
